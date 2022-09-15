57b3176b311a522f1fc70716ba0edc0d1f4219ba
chaya <chayaz.haor@gmail.com>
2022-09-15 16:16:14 -0400

    made a midi file export from random notes

--- src/main/java/piano/recorder/Recorder.java
+++ src/main/java/piano/recorder/Recorder.java
@@ -1,20 +1,22 @@
 package piano.recorder;
 
 import piano.keyboard.keyboardui.PianoLabel;
+
+import javax.sound.midi.*;
+import java.io.File;
 import java.util.ArrayList;
 
-public class Recorder
-{
+public class Recorder {
     private ArrayList<KeyPressedInfo> recordedKeysInfo = new ArrayList<>();
     private boolean isRecording;
+    private Sequence sequence;
 
-    public void append(PianoLabel labelPressed, long timePressed)
-    {
+
+    public void append(PianoLabel labelPressed, long timePressed) {
         recordedKeysInfo.add(new KeyPressedInfo(labelPressed, timePressed));
     }
 
-    void playBack()
-    {
+    void playBack() {
         long waitTime;
         for (int ix = 0; ix < recordedKeysInfo.size(); ix++)
         {
@@ -25,39 +27,92 @@ public class Recorder
                 try
                 {
                     Thread.sleep(waitTime);
-                }
-                catch (Exception exc)
+                } catch (Exception exc)
                 {
                     System.out.println(exc.getMessage());
                 }
             }
             labelPressed.play();
-            try {
+            try
+            {
                 Thread.sleep(150);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
             labelPressed.setColorToDefaultColor();
         }
+        writeMidiFile();
     }
 
-    void setIsRecording(boolean isRecording)
-    {
+    void setIsRecording(boolean isRecording) {
         this.isRecording = isRecording;
     }
 
-    public boolean getIsRecording()
-    {
+    public boolean getIsRecording() {
         return isRecording;
     }
 
-    void clearRecordedNotes()
-    {
+    void clearRecordedNotes() {
         recordedKeysInfo.clear();
     }
 
-    ArrayList<KeyPressedInfo> getRecordedKeysInfo()
-    {
+    ArrayList<KeyPressedInfo> getRecordedKeysInfo() {
         return recordedKeysInfo;
     }
+
+
+    public void writeMidiFile() {
+        try
+        {
+            sequence = new Sequence(Sequence.PPQ, 4);
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            Synthesizer synthesizer = MidiSystem.getSynthesizer();
+            synthesizer.open();
+            Track currTrack = sequence.createTrack();
+            for (int i = 5; i < (4 * 20) + 5; i += 4)
+            {
+
+                // Add Note On event
+                currTrack.add(makeASong(144, 1, i, 100, i));
+
+                // Add Note Off event
+                currTrack.add(makeASong(128, 1, i, 100, i + 2));
+            }
+
+
+            sequencer.setSequence(sequence);
+            sequencer.setTempoInBPM(120);
+            int[] allowedTypes = MidiSystem.getMidiFileTypes(sequence);
+            MidiSystem.write(sequence, allowedTypes[0], new File("C:\\temp\\o.mid"));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public MidiEvent makeASong(int command, int channel,
+                               int note, int velocity, int tick) {
+        MidiEvent event = null;
+
+        try
+        {
+            // ShortMessage stores a note as command type, channel,
+            // instrument it has to be played on and its speed.
+            ShortMessage a = new ShortMessage();
+            a.setMessage(command, channel, note, velocity);
+
+            // A midi event is composed of a short message(representing
+            // a note) and the tick at which that note has to be played
+            event = new MidiEvent(a, tick);
+        } catch (Exception ex)
+        {
+
+            ex.printStackTrace();
+        }
+        return event;
+    }
+
+
 }
